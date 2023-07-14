36efcd58972d8ab668b56d04b9ceadfd8f77701b
Alexander Koderman <ak@sernet.de>
2023-07-14 09:19:14 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# gpt.sh
-OpenAI GPT for the command line
+# Shell Script README
+
+This is a shell script that utilizes the OpenAI GPT language model to generate text based on a prompt. It provides a convenient way to interact with the GPT model and retrieve generated text.
+
+## Prerequisites
+
+Before using this script, ensure that you have the following requirements:
+
+- An OpenAI API key. You can obtain one from the OpenAI website.
+- The `jq` command-line tool installed. You can install it using your package manager (e.g., `apt`, `brew`, `yum`).
+
+## Usage
+
+To use this script, follow the steps below:
+
+1. Set your OpenAI API key by exporting it as an environment variable:
+   ```
+   export OPENAI_API_KEY=<your-api-key>
+   ```
+
+2. Run the script with the desired prompt and options:
+   ```
+   ./gpt.sh <Your_Prompt> [OPTIONS]
+   ```
+
+   For example:
+   ```
+   ./gpt.sh 'Write a bash script to split a file into 2MiB chunks.'
+   ```
+
+3. The script will generate text based on the prompt using the specified options and print the output to the console.
+
+## Options
+
+The script supports the following options:
+
+- `--model=[gpt-3.5-turbo|gpt-4|gpt-4-32k|...]`: Specify the model to use. The default is `gpt-3.5-turbo`.
+
+- `--system="You are a helpful assistant."`: Provide a system message as a primer for the model. The default is "You are a helpful assistant."
+
+## Examples
+
+- Using a system message as a primer:
+  ```
+  ./gpt.sh 'How much is the fish?' --system='You are an interpreter of techno music.'
+  ```
+
+- Using a different model:
+  ```
+  ./gpt.sh 'How much is the fish?' --model=gpt-4
+  ```
+
+- Example: use `find` to work on multiple files, applying a prompt from `prompt.txt` before. Also use a system message and the model with the largest context size:
+  ```
+  find -name "recipe_*.txt" -exec sh -c 'content=$(cat "$1"); result="$(cat ~/prompt.txt) $content"; gpt.sh "$result" --system="You are a master chef for Indian cuisine." --model=gpt-4-32k > "${1}_out.out"' sh {} \;
+  ```
+
+Make sure to replace `<your-api-key>` with your actual OpenAI API key.
+
+**Note:** The script calculates the approximate number of tokens based on the provided prompt and system message. It then sets the maximum number of tokens to generate accordingly. The default values for temperature, top_p, frequency, and presence can be modified in the script if desired.
+
+Feel free to modify and adapt this script to suit your specific needs.
+
+```
+> gpt.sh "Write an encouraging  last sentence for a github README about  a shell script."
+```
+
+"Embrace the power of automation and take your command line skills to the next level with this versatile shell script. Your productivity will soar as you simplify complex tasks and unleash your full potential."
---
+++ gpt.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+#set -x
+#set -v
+
+## use prompt.txt and call for data1, data2, data3...:
+# for file in data*; do content=$(cat $file); result="$(cat prompt.txt) $content"; gpt.sh "$result" > "${file}_out.txt"; done
+
+## use find for multiple files, with prompt from prompt.txt and system message:
+# find -name "*.txt" -exec sh -c 'content=$(cat "$1"); result="$(cat ~/prompt.txt) $content"; gpt.sh  "$result" "You are a mster chef.." > "${1}_out.out"' sh {} \;
+
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <Your_Prompt> [OPTIONS]"
+    echo ""
+    echo ""
+    echo "> gpt.sh 'Write a bash script to split a file into 2MiB chunks.'"
+    echo ""
+    echo "Using a system message as a primer:"
+    echo "> gpt.sh 'How much is the fish?' --system='You are an interpreter of techno music.' "
+    echo ""
+    echo "Using a different model:"
+    echo "> gpt.sh 'How much is the fish?' --model=gpt-4-32k"
+    echo ""
+    echo "Example using find for multiple files, for each add a prompt from prompt.txt. Also use a system message and the model with the largest context size:"
+    echo "> find -name \"recipe_*.txt\" -exec sh -c 'content=\$(cat \"\$1\"); result=\"\$(cat ~/prompt.txt) \$content\"; gpt.sh  \"\$result\" --system=\"You are a master chef for Indian cuisine.\" --model=gpt-4-32k> \"\${1}_out.out\"' sh {} \;"
+    echo ""
+    echo "Expects your API Key in the environment: export OPENAI_API_KEY=<yourkey>"
+    echo ""
+    echo "OPTIONS:"
+    echo "--model=[gpt-3.5-turbo|gpt-4|gpt-4-32k|...]"
+    echo "--system=\"You are a helpful assistant.\""
+    exit 1
+fi
+
+# Set the prompt to use as input for the language model
+PROMPT="$1"
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --model=*)
+            model="${1#*=}"
+            shift
+            ;;
+        --system=*)
+            system="${1#*=}"
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+# DEFAULTS:
+	## Set the model to use (e.g. "gpt-3.5-turbo-0301") - this is the cheapest model
+	#MODEL="gpt-3.5-turbo-0301" snapshot, deprecated 3 months after new version releases
+	#MODEL="gpt-4" 
+	#MODEL="gpt-4-32k" #4x context length
+	#MODEL="gpt-3.5-turbo"
+	MODEL="${model:-gpt-3.5-turbo}"
+SYSTEMMSG="${system:-You are a helpful assistant.}"
+
+
+# Set your OpenAI API key
+#export OPENAI_API_KEY="Your OPENAI API KEY"
+
+
+# Set the max number of tokens to generate (approx) as whole integer
+num_tokens=$(echo "$PROMPT $SYSTEMMSG" | wc -w)
+#TOKEN_COUNT=$(echo "((4000 - ($num_tokens*1.8)))/1" | bc) # for bc, 123.45/1 equals 123
+TOKEN_COUNT=$(echo "((4000 - ($num_tokens*2.6)))/1" | bc) # for bc, 123.45/1 equals 123
+
+# Set the temperature
+TEMPERATURE=0.5
+
+# Set the top_p value
+TOP_P=1
+
+# Set the frequency
+FREQUENCY=0.5
+
+# Set the presence
+PRESENCE=0.5
+
+# escape for json:
+PROMPTJSON=$(jq -n --arg multiline "$PROMPT" '$multiline | @json')
+
+
+# Generate text using the language model
+OUTPUT=$(curl -sk -X POST -H "Authorization: Bearer $OPENAI_API_KEY" -H "Content-Type: application/json" -d "{\"model\":\"$MODEL\",\"messages\":[{\"role\":\"system\",\"content\":\"$SYSTEMMSG\"},{\"role\":\"user\",\"content\": $PROMPTJSON}],\"temperature\":$TEMPERATURE,\"top_p\":$TOP_P,\"frequency_penalty\":$FREQUENCY,\"presence_penalty\":$PRESENCE,\"max_tokens\":$TOKEN_COUNT}" https://api.openai.com/v1/chat/completions)
+
+# Extract the text from the response
+OUTPUT=$(echo "$OUTPUT" | jq -r '.choices[0].message.content')
+
+# Print the output
+echo "$OUTPUT"
